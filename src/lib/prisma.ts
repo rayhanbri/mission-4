361bb10125500dd55e9311b132ebe60fadf3922f
lebwebsite02 <lebwebsite02@gmail.com>
2026-08-30 02:54:22 +0600

    many thigs added

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import { PrismaClient } from "../../generated/prisma/client";
+//eita obssoi generated folder theke anthe hobe extension theke ana jabe na 
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
@@ -8,4 +9,3 @@ const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
-
