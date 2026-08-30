ddadcbee6a05f5a1a847f7da2e2b44b9e322a819
lebwebsite02 <lebwebsite02@gmail.com>
2026-08-31 03:22:21 +0600

    basic server.ts and app.ts file created

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import app from "./app";
+
+const PORT = process.env.PORT;
+
+async function main() {
+  try {
+    console.log("Connected to the database successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+  }
+}
+
+main();
