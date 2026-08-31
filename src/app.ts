676289f0f2e42b4f421e9786ba3f96b12b6db7ba
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-01 00:21:29 +0600

    middleware setup in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
 
 const app: Application = express();
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("Hello it running")
-})
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello it running");
+});
 
 export default app;
