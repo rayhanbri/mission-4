195c65b9421fc48a9d32de611862665600e5ec3c
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-02 02:04:41 +0600

    step 4 auth

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import httpStatus from "http-status";
 import { prisma } from "./lib/prisma";
 import bcrypt from "bcryptjs";
 import { userRoutes } from "./modules/user/user.route";
+import { authRoutes } from "./modules/auth/auth.routes";
 const app: Application = express();
 
 app.use(
@@ -24,5 +25,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
