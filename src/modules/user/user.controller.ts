7bd8a7b23ddf43419b13cfa5bcd4d4c9cbfb757f
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-01 21:45:32 +0600

    user.service created

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,50 +3,12 @@ import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 import bcrypt from "bcryptjs";
 import config from "../../config";
+import { userService } from "./user.service";
 
 const registerUser = async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
+  const payload = req.body;
 
-  const isUserExist = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (isUserExist) {
-    throw new Error("User with this email already exists");
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      profilePhoto,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email || email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
+  const user = await userService.registerUserIntoDB(payload);
 
   // console.log(payload);
   //if anything change in schema
