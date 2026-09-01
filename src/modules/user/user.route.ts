992d3ad05f4ebb954ea86c0e5bd25f937f3a7853
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-01 21:36:21 +0600

    user.controller created

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,68 +1,8 @@
-import httpStatus from "http-status";
-import { Request, Response, Router } from "express";
-import { prisma } from "../../lib/prisma";
-import bcrypt from "bcryptjs";
-import config from "../../config";
+import { Router } from "express";
+import { userController } from "./user.controller";
 
 const router = Router();
 
-router.post("/register", async (req: Request, res: Response) => {
-  const { name, email, password, profilePhoto } = req.body;
-
-  const isUserExist = await prisma.user.findUnique({
-    where: { email },
-  });
-
-  if (isUserExist) {
-    throw new Error("User with this email already exists");
-  }
-
-  const hashedPassword = await bcrypt.hash(
-    password,
-    Number(config.bcrypt_salt_rounds),
-  );
-
-  const createdUser = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      profilePhoto,
-    },
-  });
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: createdUser.id,
-      email: createdUser.email || email,
-    },
-    omit: {
-      password: true,
-    },
-    include: {
-      profile: true,
-    },
-  });
-
-  // console.log(payload);
-  //if anything change in schema
-  //1.npx prisma migrate dev
-  //2. npx prisma generate
-
-  res.status(httpStatus.CREATED).json({
-    success: true,
-    statusCode: httpStatus.CREATED,
-    message: "User registered successfully",
-    data: {
-      user,
-    },
-  });
-});
+router.post("/register", userController.registerUser);
 
 export const userRoutes = router;
