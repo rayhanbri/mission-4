e5f4a0a1f1ec99b49947a8fd18830699dedcf141
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-02 02:50:24 +0600

    utils function for jwt

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
 import jwt, { SignOptions } from "jsonwebtoken";
 import config from "../../config";
+import { jwtUtils } from "../../utils/jwtUtils";
 
 const loginUser = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -24,14 +25,26 @@ const loginUser = async (payload: ILoginUser) => {
     role: user.role,
   };
 
-  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
-    expiresIn: config.jwt_access_expires_in,
-  } as SignOptions);
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
 
-  const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret, {
-    expiresIn: config.jwt_refresh_expires_in,
-  } as SignOptions);
+  // const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
+  //   expiresIn: config.jwt_access_expires_in,
+  // } as SignOptions);
+
+  // const refreshToken = jwt.sign(jwtPayload, config.jwt_refresh_secret, {
+  //   expiresIn: config.jwt_refresh_expires_in,
+  // } as SignOptions);
   // why we are usign  signoptions
+
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
 
   return {
     accessToken,
