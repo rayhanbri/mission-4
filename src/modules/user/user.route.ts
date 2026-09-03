c291b2e6b4bc03119fe4aaa9dd0f949c5c87ecc1
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-03 20:04:07 +0600

    now we stopping use by checking role

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,5 +1,10 @@
-import { Request, Response, Router } from "express";
+import { httpStatus } from "http-status";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
+import { jwtUtils } from "../../utils/jwtUtils";
+import config from "../../config";
+import { Role } from "../../../generated/prisma/enums";
+import { sendResponse } from "../../utils/sendResponse";
 
 const router = Router();
 
@@ -7,8 +12,26 @@ router.post("/register", userController.registerUser);
 
 router.get(
   "/me",
-  (req: Request, res: Response) => {
-    res.send("i am middleware");
+  (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    );
+
+    if (typeof verifiedToken === "string") {
+      throw new Error(verifiedToken);
+    }
+
+    const { email, name, id, role } = verifiedToken;
+    const requiredRoles = [Role.ADMIN, Role.AUTHOR, Role.USER];
+
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+      res.send("this is forbidden ");
+    }
+
+    next();
   },
   userController.getMyProfile,
 );
