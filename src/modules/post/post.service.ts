c703d2f77c79bba0c39b649d96717d712744b8e3
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-12 01:02:58 +0600

    post service folder sturcutre created

---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,23 @@
+const createPost = async () => {};
+
+const getAllPosts = async () => {};
+
+const getPostById = async () => {};
+
+const updatePost = async () => {};
+
+const deletePost = async () => {};
+
+const getPostsStats = async () => {};
+
+const getMyPosts = async (authorId: string) => {};
+
+export const postService = {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  getPostsStats,
+  getMyPosts,
+};
