cdcdd41260a24f8da47ffa78b707c552faa7e5e1
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-15 03:40:32 +0600

    update post api created

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -54,7 +54,32 @@ const getPostById = catchAsync(
 );
 
 const updatePost = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.user?.id;
+    const isAdmin = req.user?.role === "ADMIN";
+
+    const postId = req.params.postId;
+
+    if (!postId) {
+      throw new Error("Post Id Required In Params");
+    }
+
+    const payload = req.body;
+
+    const result = await postService.updatePost(
+      postId as string,
+      payload,
+      authorId as string,
+      isAdmin,
+    );
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Post updated successfully",
+      data: result,
+    });
+  },
 );
 
 const deletePost = catchAsync(
--- src/modules/post/post.interface.ts
+++ src/modules/post/post.interface.ts
@@ -1,11 +1,20 @@
 import { PostStatus } from "../../../generated/prisma/enums";
 
 export interface ICreatePostPayload {
-    title : string;
-    content : string;
-    thumbnail ?: string;
-    isFeatured ?: boolean;
-    isPremium ?: boolean
-    status ?: PostStatus
-    tags : string[]
+  title: string;
+  content: string;
+  thumbnail?: string;
+  isFeatured?: boolean;
+  isPremium?: boolean;
+  status?: PostStatus;
+  tags: string[];
+}
+
+export interface IUpdatePostPayload {
+  title?: string;
+  content?: string;
+  thumbnail?: string;
+  isFeatured?: boolean;
+  status?: PostStatus;
+  tags?: string[];
 }
