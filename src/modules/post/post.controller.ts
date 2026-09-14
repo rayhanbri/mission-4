3599e440a256e63a444422e6b861b55df91c5a88
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-15 03:27:29 +0600

    get my post api created

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -66,7 +66,18 @@ const getPostsStats = catchAsync(
 );
 
 const getMyPosts = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.user?.id;
+
+    const result = await postService.getMyPosts(authorId as string);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "My Posts retrieved successfuly",
+      data: result,
+    });
+  },
 );
 
 export const postController = {
