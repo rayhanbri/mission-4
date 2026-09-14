0556a24f17820c152500090872cfba8937d8a9c7
lebwebsite02 <lebwebsite02@gmail.com>
2026-09-14 22:58:57 +0600

    get post by id is  created

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -35,7 +35,22 @@ const getAllPosts = catchAsync(
 );
 
 const getPostById = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {},
+  async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.postId;
+
+    if (!postId) {
+      throw new Error("Post Id Required In Params");
+    }
+
+    const result = await postService.getPostById(postId as string);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Post retrieved successfuly",
+      data: result,
+    });
+  },
 );
 
 const updatePost = catchAsync(
